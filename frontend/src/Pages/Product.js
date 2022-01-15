e721cf25f466a5a4e9297a239c223e766766e88b
mathieuelisa <mathieu.elisa@gmail.com>
2022-01-15 15:40:50 +0100

    Adding products page and styling

--- frontend/src/Pages/Product.js
+++ frontend/src/Pages/Product.js
@@ -6,9 +6,25 @@ function Product() {
 
   const myProduct = products.find((p) => p._id === id);
   return (
-    <>
-      <h1>{myProduct.name}</h1>
-    </>
+    <div className="product__container">
+      <div className="product__container-pictures">
+        <img
+          className="product__container-pictures-img"
+          src={myProduct.image}
+          alt="test"
+        />
+      </div>
+      <div className="product__container-infos">
+        <h1 className="product__container-infos-name">{myProduct.name}</h1>
+        <h3 className="product__container-infos-price">€ {myProduct.price}</h3>
+        <p className="product__description">Description</p>
+        <p className="product__container-infos-text">{myProduct.description}</p>
+
+        <div>
+          <button>Valider votre achat</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
