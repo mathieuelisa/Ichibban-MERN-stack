3abed51f87a979192decfab18a63c8ab5e519275
mathieuelisa <mathieu.elisa@gmail.com>
2022-01-23 13:09:21 +0100

    Adding users data

---
+++ backend/data/users.js
@@ -0,0 +1,20 @@
+const users = [
+  {
+    name: "Admin",
+    email: "admin@exemple.fr",
+    password: xxxxx,
+    isAdmin: true,
+  },
+  {
+    name: "Johnny",
+    email: "johnny@exemple.fr",
+    password: xxxxx,
+  },
+  {
+    name: "Jeannot",
+    email: "jeannot@exemple.fr",
+    password: xxxxx,
+  },
+];
+
+export default users;
