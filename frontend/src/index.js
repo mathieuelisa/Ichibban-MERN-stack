76acd0bcfcc9948d80b35475ab0eea21177637f7
mathieuelisa <mathieu.elisa@gmail.com>
2022-01-25 23:52:18 +0100

    Starting the setting of redux in the apps

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
 import "./index.scss";
 import App from "./App";
 
+// Store React
+import store from "./redux/store";
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
+
   document.getElementById("root")
 );
