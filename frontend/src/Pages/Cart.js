457aaef10c9565e22935c73c1f6697ddda63684e
mathieuelisa <mathieu.elisa@gmail.com>
2022-02-01 21:06:28 +0100

    get id and quantity in url

--- frontend/src/Pages/Cart.js
+++ frontend/src/Pages/Cart.js
@@ -1,7 +1,16 @@
+import { useLocation, useParams } from "react-router-dom";
+
 function Cart() {
+  const { id } = useParams();
+  const location = useLocation();
+
+  const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+
+  console.log(qty);
+
   return (
     <>
-      <h2>Cart page !!!</h2>
+      <h2>Cart page !!! {id}</h2>
     </>
   );
 }
