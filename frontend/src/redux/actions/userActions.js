46af9e69f9a7fdac8819788ad04f7a489241aac2
mathieuelisa <mathieu.elisa@gmail.com>
2022-02-13 20:45:23 +0100

    Adding user actions and userReducers

---
+++ frontend/src/redux/actions/userActions.js
@@ -0,0 +1,4 @@
+export const USER_LOGIN_REQUEST = "user_login_request";
+export const USER_LOGIN_SUCCESSFUL = "user_login_successful";
+export const USER_LOGIN_FAIL = "user_login_fail";
+export const USER_LOGOUT = "user_logout";
--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -3,12 +3,14 @@ import { combineReducers } from "redux";
 // Reducers
 import { productsReducer, productDetailReducer } from "./productsReducer";
 import { cartReducer } from "./cartReducer";
+import { userLoginReducer } from "./userReducer";
 
 // Setting each reducers
 const reducers = combineReducers({
   ListProducts: productsReducer,
   ListDetailProduct: productDetailReducer,
   Cart: cartReducer,
+  UserLogin: userLoginReducer,
 });
 
 export default reducers;
