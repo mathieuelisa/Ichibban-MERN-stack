59c56f96934fb5c30bc6e7703272d532862da3fd
mathieuelisa <mathieu.elisa@gmail.com>
2022-02-24 11:38:29 +0100

    Adding first part of order page with some effects

--- frontend/src/Pages/Order.js
+++ frontend/src/Pages/Order.js
@@ -1,6 +1,11 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import Steps from "../Components/Steps";
 
 function Order() {
+  const cart = useSelector((state) => state.Cart);
+  const { cartItems, shippingAddress, paymentMethod } = cart;
+
   return (
     <>
       <Steps step1 step2 step3 step4 />
@@ -8,15 +13,55 @@ function Order() {
         <h2 className="order__title">Resumé de votre commande</h2>
         <div className="order__container-mainTitle">
           <div className="order__container-parts-articles">
-            <h4>Liste des articles selectionnés</h4>
+            <h4>Liste des articles selectionnés :</h4>
+            <div className="order__container_elements">
+              {cartItems.map((element, index) => {
+                return (
+                  <div className="order__container_elements-each" key={index}>
+                    <img
+                      src={element.image}
+                      className="order__container-image"
+                      alt="EachProduct"
+                    ></img>
+                    <Link
+                      to={`/product/${element.product}`}
+                      className="order__container-nameOfProduct"
+                    >
+                      {element.name}
+                    </Link>
+                    <h3>
+                      {element.quantity} x {element.price} € ={" "}
+                      {element.quantity * element.price} €
+                    </h3>
+                  </div>
+                );
+              })}
+            </div>
           </div>
 
-          <div className="order__container-parts">
-            <h4>Adresse de livraison</h4>
+          <div className="order__container-parts-shipping">
+            <h4>Votre adresse de livraison :</h4>
+            <div className="order__container_elements-shipping">
+              <h4 className="order__container-parts-shipping-title">
+                {shippingAddress.address}
+              </h4>
+              <h4 className="order__container-parts-shipping-title">
+                {shippingAddress.city}
+              </h4>
+              <h4 className="order__container-parts-shipping-title">
+                {shippingAddress.postalCode}
+              </h4>
+              <h4 className="order__container-parts-shipping-title">
+                {shippingAddress.country}
+              </h4>
+            </div>
           </div>
 
           <div className="order__container-parts-paymentMethod">
-            <h4>Method de paiment</h4>
+            <h4>Votre methode de paiement :</h4>
+            <h4 className="order__container-parts-paymentMethod-title">
+              {paymentMethod}
+            </h4>
           </div>
         </div>
       </div>
